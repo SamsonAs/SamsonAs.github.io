0205704cdb1eae6917ee8b92148ec6224399b9bf
Samson Asher <kaptankarps@gmail.com>
2016-04-30 16:25:36 -0400

    save

--- flappy/skilstak-flappy.js
+++ flappy/skilstak-flappy.js
@@ -70,7 +70,7 @@ var CONFIG = {
     } 
   },
   "gameover": {
-    "text": "Game\nOver",
+    "text": "You were crushed\nLike the capitalist scum!",
     "offset": -150,
     "style": {
       "font": "100px sans-serif", 
